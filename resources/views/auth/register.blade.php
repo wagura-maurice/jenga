addc5816b25e89c7b2f3c1e15dd53476afb7c85b
Wagura Maurice <business@waguramaurice.com>
2025-07-12 17:09:22 +0300

    update 10 files

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
 @extends('admin.layouts.app')
 
-@section('content')    
+@section('content')
     <!-- begin #page-loader -->
     <div id="page-loader" class="fade in"><span class="spinner"></span></div>
     <!-- end #page-loader -->
- 
+
     <!-- begin #page-container -->
     <div id="page-container" class="fade">
         <!-- begin register -->
@@ -17,7 +17,9 @@
                 <div class="news-caption">
                     <h4 class="caption-title"><i class="fa fa-edit text-success"></i> Announcing the Color Admin app</h4>
                     <p>
-                        As a Color Admin Apps administrator, you use the Color Admin console to manage your organization’s account, such as add new users, manage security settings, and turn on the services you want your team to access.
+                        As a Color Admin Apps administrator, you use the Color Admin console to manage your organization’s
+                        account, such as add new users, manage security settings, and turn on the services you want your
+                        team to access.
                     </p>
                 </div>
             </div>
@@ -37,37 +39,44 @@
                         <label class="control-label">Name <span class="text-danger">*</span></label>
                         <div class="row row-space-10">
                             <div class="col-md-12">
-                                <input type="text" class="form-control" placeholder="Name" name="name" value="{{ old('name') }}" required autofocus />
+                                <input type="text" class="form-control" placeholder="Name" name="name"
+                                    value="{{ old('name') }}" required autofocus />
                             </div>
                         </div>
                         <label class="control-label">Email <span class="text-danger">*</span></label>
                         <div class="row m-b-15">
                             <div class="col-md-12">
-                                <input type="text" class="form-control" placeholder="Email address" name="email" value="{{ old('email') }}" required/>
+                                <input type="text" class="form-control" placeholder="Email address" name="email"
+                                    value="{{ old('email') }}" required />
                             </div>
                         </div>
                         <label class="control-label">User Account <span class="text-danger">*</span></label>
                         <div class="row m-b-15">
                             <div class="col-md-12">
-                                <input type="text" class="form-control" placeholder="Email address" name="email" value="{{ old('email') }}" required/>
+                                <input type="text" class="form-control" placeholder="Email address" name="email"
+                                    value="{{ old('email') }}" required />
                             </div>
-                        </div>                        
+                        </div>
                         <label class="control-label">Password <span class="text-danger">*</span></label>
                         <div class="row m-b-15">
                             <div class="col-md-12">
-                                <input type="password" class="form-control" placeholder="Password" name="password" required />
+                                <input type="password" class="form-control" placeholder="Password" name="password"
+                                    required />
                             </div>
                         </div>
                         <label class="control-label">Confirm Password <span class="text-danger">*</span></label>
                         <div class="row m-b-15">
                             <div class="col-md-12">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required />
+                                <input id="password-confirm" type="password" class="form-control"
+                                    name="password_confirmation" required />
                             </div>
                         </div>
 
                         <div class="checkbox m-b-30">
                             <label>
-                                <input type="checkbox" required /> By clicking Sign Up, you agree to our <a href="#">Terms</a> and that you have read our <a href="#">Data Policy</a>, including our <a href="#">Cookie Use</a>.
+                                <input type="checkbox" required /> By clicking Sign Up, you agree to our <a
+                                    href="#">Terms</a> and that you have read our <a href="#">Data Policy</a>,
+                                including our <a href="#">Cookie Use</a>.
                             </label>
                         </div>
                         <div class="register-buttons">
@@ -87,19 +96,31 @@
             <!-- end right-content -->
         </div>
         <!-- end register -->
-        
+
         <!-- begin theme-panel -->
         <div class="theme-panel">
             <a href="javascript:;" data-click="theme-panel-expand" class="theme-collapse-btn"><i class="fa fa-cog"></i></a>
             <div class="theme-panel-content">
                 <h5 class="m-t-0">Color Theme</h5>
                 <ul class="theme-list clearfix">
-                    <li class="active"><a href="javascript:;" class="bg-green" data-theme="default" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Default">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-red" data-theme="red" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Red">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-blue" data-theme="blue" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Blue">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-purple" data-theme="purple" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Purple">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-orange" data-theme="orange" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Orange">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-black" data-theme="black" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Black">&nbsp;</a></li>
+                    <li class="active"><a href="javascript:;" class="bg-green" data-theme="default"
+                            data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body"
+                            data-title="Default">&nbsp;</a></li>
+                    <li><a href="javascript:;" class="bg-red" data-theme="red" data-click="theme-selector"
+                            data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Red">&nbsp;</a>
+                    </li>
+                    <li><a href="javascript:;" class="bg-blue" data-theme="blue" data-click="theme-selector"
+                            data-toggle="tooltip" data-trigger="hover" data-container="body"
+                            data-title="Blue">&nbsp;</a></li>
+                    <li><a href="javascript:;" class="bg-purple" data-theme="purple" data-click="theme-selector"
+                            data-toggle="tooltip" data-trigger="hover" data-container="body"
+                            data-title="Purple">&nbsp;</a></li>
+                    <li><a href="javascript:;" class="bg-orange" data-theme="orange" data-click="theme-selector"
+                            data-toggle="tooltip" data-trigger="hover" data-container="body"
+                            data-title="Orange">&nbsp;</a></li>
+                    <li><a href="javascript:;" class="bg-black" data-theme="black" data-click="theme-selector"
+                            data-toggle="tooltip" data-trigger="hover" data-container="body"
+                            data-title="Black">&nbsp;</a></li>
                 </ul>
                 <div class="divider"></div>
                 <div class="row m-t-10">
@@ -158,7 +179,8 @@
                 </div>
                 <div class="row m-t-10">
                     <div class="col-md-12">
-                        <a href="#" class="btn btn-inverse btn-block btn-sm" data-click="reset-local-storage"><i class="fa fa-refresh m-r-3"></i> Reset Local Storage</a>
+                        <a href="#" class="btn btn-inverse btn-block btn-sm" data-click="reset-local-storage"><i
+                                class="fa fa-refresh m-r-3"></i> Reset Local Storage</a>
                     </div>
                 </div>
             </div>
